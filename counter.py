c902a686d22457c7aa76a3ee8a61e79f2b01e893
Lisa Collins <Lisa.Collins@veteransunited.com>
2025-02-25 10:04:20 -0600

    LisaCollinsDaysOfWeek

--- counter.py
+++ counter.py
@@ -1,7 +1,16 @@
 def countDayOfTheWeek():
     # This first line is provided for you
     file_name = input("Enter a file name: ")
-
+    fhand = open(file_name)
+    counter = {}
+    for line in fhand:
+        if line.startswith("From"):
+            line = line.split()
+            try:
+                if line[2] not in counter: counter[line[2]] = 1
+                else: counter[line[2]] = counter[line[2]] + 1
+            except: next
+    print(counter)
 
 
 ## if you want to test locally run > python payCalculator.py
